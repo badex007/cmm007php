5fceff87fb433e440e7ad6feac357031bc6f1938
badex007 <99981331+badex007@users.noreply.github.com>
2024-03-17 07:24:24 +0100

    first php commit
    hello php!

---
+++ Lab_7/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo php</title>
+</head>
+
+<body>
+    <p>
+    <h1>
+        <?php
+        echo "Hello World";
+        ?>
+    </h1>
+
+    </p>
+
+</body>
+
+</html>
