afff39574776dd4ed38ee5a49bdda916f44ccd7d
badex007 <99981331+badex007@users.noreply.github.com>
2024-04-06 19:12:39 +0100

    updated

--- Lab_7/index.php
+++ Lab_7/index.php
@@ -11,10 +11,30 @@
     <p>
     <h1>
         <?php
-        echo "Hello World";
+        echo "Hello World ";
+        echo "Hello,".""."World"."! ";
+        echo 5*7;
+        $myname = "frod";
+        $myage = 111;
+        echo " My name is ".$myname."and i am ".$myage;
         ?>
     </h1>
 
+    </p>
+
+    <p>
+        <?php
+
+        $name = "Edgar";
+
+        if($name == "Simon"){
+            print "i know you!";
+        }
+        else{
+            print "Who are you?";
+        }
+
+        ?>
     </p>
 
 </body>
